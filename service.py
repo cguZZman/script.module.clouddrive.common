dba6c676d1b7a78509f4dccfd0c9f738afb155ed
cguzmangarcia <cguzmangarcia@USMECILIESLAP19.us.deloitte.com>
2017-10-13 00:18:42 -0400

    service added

--- service.py
+++ service.py
@@ -19,73 +19,7 @@
     Created on Mar 1, 2015
     @author: Carlos Guzman (cguZZman) carlosguzmang@hotmail.com
 '''
-import SocketServer
-import socket
-import threading
-
-from clouddrive.common.service.download import Download
-from clouddrive.common.service.source import Source
-from clouddrive.common.ui.logger import Logger
-import xbmc
-import xbmcaddon
-
-
-class CloudDriveService(object):
-    _interface = '127.0.0.1'
-    _common_addon = xbmcaddon.Addon('script.module.clouddrive.common')
-    
-    def __init__(self):
-        SocketServer.TCPServer.allow_reuse_address = True
-    
-    def unused_port(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self._interface, 0))
-        port = sock.getsockname()[1]
-        sock.close()
-        return port
-    
-    def start_download_server(self):
-        download_service_port = self.unused_port()
-        self._common_addon.setSetting('download.service.port', str(download_service_port))
-        Logger.notice('Cloud Drive Common Download Service Port: ' + str(download_service_port))
-        download_server = SocketServer.TCPServer((self._interface, download_service_port), Download)
-        download_server.server_activate()
-        download_server.timeout = 1
-        download_service_thread = threading.Thread(target=download_server.serve_forever)
-        download_service_thread.daemon = True
-        download_service_thread.start()
-        return download_server
-
-    def start_source_server(self):
-        if self._common_addon.getSetting('allow_directory_listing') == 'true':
-            source_service_port = int(self._common_addon.getSetting('port_directory_listing'))
-            source_server = SocketServer.TCPServer((self._interface, source_service_port), Source)
-            source_server.server_activate()
-            source_server.timeout = 1
-            source_service_thread = threading.Thread(target=source_server.serve_forever)
-            source_service_thread.daemon = True
-            source_service_thread.start()
-            Logger.notice('Cloud Drive Common Source Service Port: ' + str(source_service_port))
-            return source_server
-        
-    def main(self):
-        download_server = self.start_download_server()
-        source_server = self.start_source_server()
-        monitor = xbmc.Monitor()
-        while not monitor.abortRequested():
-            if monitor.waitForAbort(2):
-                download_server.shutdown()
-                if source_server:
-                    source_server.shutdown()
-                break
-        download_server.server_close()
-        download_server.socket.close()
-        download_server.shutdown()
-        if source_server:
-            source_server.server_close()
-            source_server.socket.close()
-            source_server.shutdown()
-        Logger.notice('Cloud Drive Common Services Stopped.')
+from clouddrive.common.service.clouddriveservice import CloudDriveService
 
 
 if __name__ == '__main__':
